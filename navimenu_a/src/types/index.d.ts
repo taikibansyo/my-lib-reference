0f57bc0bad333264f56ef44118c94a7e982d15f5
taikibansyo <taikibansyo@gmail.com>
2025-05-25 17:21:10 +0900

    ファイル構成を整理

--- navimenu_a/src/types/index.d.ts
+++ navimenu_a/src/types/index.d.ts
@@ -1,12 +1,20 @@
-import { StaticNaviMenu } from "./staticNaviMenu/StaticNaviMenu";
-
+import { Settings, Dom, PrevDirection } from "./staticNaviMenu";
+declare class StaticNaviMenu {
+  DOM: Dom;
+  circleDiameter: number;
+  circleInterval: number;
+  previousIndex: number;
+  prevDirection: PrevDirection;
+  constructor(settings: Settings);
+  private _getElements;
+  private _getElement;
+  private _setMoveX;
+  private _setStyleWidth;
+  private _setValueAsync;
+  private _toggleClass;
+  private _animateIndicatorToRight;
+  private _animateIndicatorToLeft;
+  private _toggle;
+  addEvent(): void;
+}
 export { StaticNaviMenu };
-
-export type {
-  Settings,
-  Dom,
-  SetObjects,
-  MoveObjects,
-  NextDirection,
-  PrevDirection,
-} from "./types/staticNaviMenu";
