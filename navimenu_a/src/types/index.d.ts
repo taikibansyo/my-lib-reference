54f80e1f63542947639a1efd154f1286ed7cebd5
taikibansyo <taikibansyo@gmail.com>
2025-05-25 16:36:42 +0900

    ファイル構成を整理

---
+++ navimenu_a/src/types/index.d.ts
@@ -0,0 +1,12 @@
+import { StaticNaviMenu } from "./staticNaviMenu/StaticNaviMenu";
+
+export { StaticNaviMenu };
+
+export type {
+  Settings,
+  Dom,
+  SetObjects,
+  MoveObjects,
+  NextDirection,
+  PrevDirection,
+} from "./types/staticNaviMenu";
